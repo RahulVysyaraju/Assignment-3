7b5227172451852e6c52db2d32381fb392de7244
RahulVysyaraju <rahulvysyaraju27@gmail.com>
2025-06-04 15:15:30 +0530

    Add files via upload

---
+++ Assignment 3.2.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number: "))
+
+from math import *
+print("Square root: ",sqrt(a))
+print("Logarithm: ",log(a))
+print("Sine: ",sin(a))
+
+print("\nAlternate way but same answer\n")
+
+print("Square root: ",sqrt(a),"\nLogarithm: ",log(a),"\nSine: ",sin(a))
